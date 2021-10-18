a278cebabc91a1d36f428c486c35f128194cebe4
wang_lei <l.wang@s2icode.com>
2021-10-18 21:36:40 +0800

    项目创建阶段

---
+++ springboot_project/demo/demo/src/main/java/com/wanger/demo/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.wanger.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String sayHell0() {
+        return "Hello World";
+    }
+}
